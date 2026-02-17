99242c1c183e199e3b43eaa19cb07fc2e16b2ae6
KRISHIKA <krishika1400@gmail.com>
2026-02-18 00:37:33 +0530

    Refactor showSection and event listener syntax

--- script.js
+++ script.js
@@ -8,37 +8,37 @@ const thankYou = document.getElementById("thankYou");
 let current = 0;
 let submitted = false;
 
-function showSection(index){
+function showSection(index) {
   sections.forEach(sec => sec.classList.remove("active"));
   sections[index].classList.add("active");
-  progressBar.style.width = ((index+1)/sections.length)*100 + "%";
+  progressBar.style.width = ((index + 1) / sections.length) * 100 + "%";
 }
 
-nextBtns.forEach(btn=>{
-  btn.addEventListener("click",()=>{
-    if(current < sections.length-1){
+nextBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    if (current < sections.length - 1) {
       current++;
       showSection(current);
     }
   });
 });
 
-prevBtns.forEach(btn=>{
-  btn.addEventListener("click",()=>{
-    if(current > 0){
+prevBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    if (current > 0) {
       current--;
       showSection(current);
     }
   });
 });
 
-document.querySelector("iframe").addEventListener("load",()=>{
-  if(submitted){
-    form.style.display="none";
+document.querySelector("iframe").addEventListener("load", () => {
+  if (submitted) {
+    form.style.display = "none";
     thankYou.classList.remove("hidden");
   }
 });
 
-form.addEventListener("submit",()=>{
+form.addEventListener("submit", () => {
   submitted = true;
 });
